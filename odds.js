9ea017c09f9fa5e5adcd87272f2f57348129519c
sanxiao686-byte <sanxiao686@outlook.com>
2026-09-12 15:38:18 +0800

    Load 500.com real odds chunks; cache-bust v=36

---
+++ odds.js
@@ -0,0 +1,4 @@
+/* 500.com XML snapshot 2026-09-12 15:33:49. No invented numbers. */
+var REAL_ODDS = Object.assign({}, window.RO0||{}, window.RO1||{}, window.RO2||{});
+var JC_LIVE = {};
+var ODDS_META = {source:"500.com XML", updated:"2026-09-12 15:33:49", note:"即时欧盘/亚盘/凯利来自500网；初盘/必发成交/xG未编造"};
