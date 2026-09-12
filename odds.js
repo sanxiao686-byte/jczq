1961ceac32a0ab7086710b415e1101f48828763b
sanxiao686-byte <sanxiao686@outlook.com>
2026-09-12 18:31:19 +0800

    feat: load 500.com real euro/kelly/asian odds (v38 cache bust, odds0-2)

--- odds.js
+++ odds.js
@@ -1,4 +1,6 @@
-/* 500.com XML snapshot 2026-09-12 15:33:49. No invented numbers. */
+/* 500.com XML 2026-09-12 15:33:49. Real instant odds. No invented numbers. */
 var REAL_ODDS = Object.assign({}, window.RO0||{}, window.RO1||{}, window.RO2||{});
-var JC_LIVE = {};
+var JC_LIVE = window.JC_LIVE || {};
 var ODDS_META = {source:"500.com XML", updated:"2026-09-12 15:33:49", note:"即时欧盘/亚盘/凯利来自500网；初盘/必发成交/xG未编造"};
+window.REAL_ODDS = REAL_ODDS;
+window.JC_LIVE = JC_LIVE;
