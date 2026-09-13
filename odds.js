399fd5764dc6247f8403c2d784775e122437c157
sanxiao686-byte <sanxiao686@outlook.com>
2026-09-13 13:25:05 +0800

    Sunday 2026-09-13 full 24 matches 500.com instant odds v40

--- odds.js
+++ odds.js
@@ -1,4 +1,4 @@
 /* Merge 500.com packs. Do not overwrite RO0/RO1/RO2. */
 window.REAL_ODDS = Object.assign({}, window.RO0||{}, window.RO1||{}, window.RO2||{}, window.REAL_ODDS||{});
 window.JC_LIVE = window.JC_LIVE || {};
-window.ODDS_META = {source:"500.com XML", updated:"2026-09-12 15:33:49", note:"即时欧盘/亚盘/凯利；初盘/必发/xG未编造"};
+window.ODDS_META = {source:"500.com XML", updated:"2026-09-13 13:24:05", note:"即时欧盘/亚盘/凯利；初盘/必发/xG未编造"};
